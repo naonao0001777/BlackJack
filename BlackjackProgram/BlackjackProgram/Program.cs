a15463b8c202d511faf135feb7f6dd4778fae29c
NAO <46675984+naonao0001777@users.noreply.github.com>
2019-01-22 21:23:27 +0900

    Update Program.cs

--- BlackjackProgram/BlackjackProgram/Program.cs
+++ BlackjackProgram/BlackjackProgram/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +15,15 @@ namespace BlackjackProgram
             CPU cpu = new CPU();
             cpu.draw();
 
-            Console.WriteLine("カードを引いてください。(Y/N)");
+            Player player = new Player();
+            while (player.point<=21)
+            {
+                Console.WriteLine("カードを引きますか？(Y/N)");
                 string option = Console.ReadLine();
 
                 if (option == "y" || option == "Y")
                 {
-                    Player player = new Player();
                     player.draw();
-                
 
                 }
                 else if (option == "n" || option == "N")
@@ -33,31 +34,38 @@ namespace BlackjackProgram
                 {
                     Console.Write("想定外の文字列が入力されました。");
                 }
+            }
            
-                Console.ReadKey();
+            Console.ReadKey();
         }
     }
     public class CardDraw
     {
-        public string []marks = new string[4] { "スペード", "クラブ", "ダイヤ", "ハート" };
+        public string[] marks = new string[4] { "スペード", "クラブ", "ダイヤ", "ハート" };
         public string []number = new string [13] {"1","2","3","4","5","6","7","8","9","10","11","12","13" };
 
-        Random r1 = new System.Random();
-        int random_m = r1.Next(3);
+        public void draw()
+        {
+            Random r1 = new System.Random();
+            int random_m = r1.Next(3);
 
-        Random r2 = new System.Random();
-        int random_n = r2.Next(12);
+            Random r2 = new System.Random();
+            int random_n = r2.Next(12);
 
-
-        string mark = marks[random_m];
-        string number = number[random_n];
-
-        Console.WriteLine("CPUが引いたのは{0}の{1}",mark,number);
+            CardDraw cd = new CardDraw();
+            string mark = cd.marks[random_m];
+            string number = cd.number[random_n];
+        }
+        //Console.WriteLine("CPUが引いたのは{0}の{1}",mark,number);
     }
+
     public class CPU
     {
         public string mark { set; get; }
+
         public string number { set; get; }
+
+        public int point { set; get; }
 
         public void draw()
         {
@@ -73,13 +81,19 @@ namespace BlackjackProgram
             string mark = card.marks[random_m];
             string number = card.number[random_n];
 
+            CPU cpu = new CPU();
+            cpu.point = int.Parse(number);
+
             Console.WriteLine("CPUが引いたのは{0}の{1}",mark,number);
         }
     }
     public class Player
     {
         public string mark { set; get; }
-        public string number { set; get; }
+
+        public string number { set; get; } 
+
+        public int point { set; get; }
 
         public void draw()
         {
@@ -94,6 +108,9 @@ namespace BlackjackProgram
             string mark = card.marks[random_m];
             string number = card.number[random_n];
 
+            Player player = new Player();
+            player.point = int.Parse(number);
+
             Console.WriteLine("あなたが引いたのは{0}の{1}", mark, number);
         }
     }
@@ -107,10 +124,6 @@ namespace BlackjackProgram
             Player player = new Player();
             int pleyerNum = int.Parse(player.number);
 
-            if (cpuNum)
-            {
-
-            }
 
         }
     }
